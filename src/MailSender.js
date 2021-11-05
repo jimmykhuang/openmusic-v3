19c49c721100c51b29ed8ff0ad648db866da7744
jimmykhuang <jimmy_khuang@hotmail.co.id>
2021-11-05 14:28:32 +0700

    error

--- src/MailSender.js
+++ src/MailSender.js
@@ -18,7 +18,8 @@ class MailSender {
 
     sendEmail(targetEmail, content) {
         const message = {
-            from: 'Notes Apps',
+
+            from: 'Songs Apps',
             to: targetEmail,
             subject: 'Ekspor Catatan',
             text: 'Terlampir hasil dari ekspor catatan',
--- src/SongsService.js
+++ src/SongsService.js
@@ -8,17 +8,21 @@ class SongsService {
     }
 
 
-    async getSongs(userId) {
+    async getSongs(playlistId) {
         const query = {
             //         text: `SELECT notes.* FROM notes
             //   LEFT JOIN collaborations ON collaborations.note_id = notes.id
             //   WHERE notes.owner = $1 OR collaborations.user_id = $1
             //   GROUP BY notes.id`,
-            text: `SELECT playlists.* FROM playlists
-      LEFT JOIN collaborations ON collaborations.playlist_id = playlists.id
-      WHERE playlists.owner = $1 OR collaborations.playlist_id = $1
-      GROUP BY playlists.id`,
-            values: [userId],
+            //         text: `SELECT playlists.* FROM playlists
+            //   LEFT JOIN collaborations ON collaborations.playlist_id = playlists.id
+            //   WHERE playlists.owner = $1 OR collaborations.playlist_id = $1
+            //   GROUP BY playlists.id`,
+            //         values: [userId],
+            text: ` SELECT songs.id, songs.title, songs.performer FROM playlistsongs
+            JOIN songs ON songs.id = playlistsongs.song_id
+            WHERE playlistsongs.playlist_id = $1`,
+            values: [playlistId],
         };
         const result = await this._pool.query(query);
         return result.rows;
